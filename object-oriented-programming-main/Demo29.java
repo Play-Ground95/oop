bd64ad0127637fb02c682c450ecbb26093029fd9
Kavindu95 <kavindueng95@gmail.com>
2023-09-12 19:10:23 +0530

    initial commit

--- object-oriented-programming-main/Demo29.java
+++ object-oriented-programming-main/Demo29.java
@@ -1,14 +1,51 @@
+import java.util.Scanner;
+
 public class Demo29 {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String msg;
         do{
+            String msg;
+            do {
+                System.out.print("Enter your message: ");
+                msg = SCANNER.nextLine();
+            } while (msg.isBlank());
 
-        }while(false);
+            System.out.println();
+            System.out.println("1. File");
+            System.out.println("2. Console");
+            int option;
+            do {
+                System.out.print("Where do you want to write your message? ");
+                option = SCANNER.nextInt(); SCANNER.nextLine();
+            } while (option < 1 || option > 2);
 
-        System.out.println("Where do you want to write your message?");
-        
+            //MyWriter writer;
 
+            if (option == 1){
+                String filename;
+                do {
+                    System.out.print("Enter a file name: ");
+                    filename = SCANNER.nextLine();
+                }while (filename.isBlank());
+                //writer = new MyFileWriter(filename);
+            } else {
+                System.out.println("1. RED");
+                System.out.println("2. GREEN");
+                System.out.println("3. YELLOW");
+                System.out.println("4. BLUE");
+                int colorOption;
+                do {
+                    System.out.print("Enter your color option: ");
+                    colorOption = SCANNER.nextInt(); SCANNER.nextLine();
+                } while (colorOption < 1 || colorOption > 4);
+                String[] colors = {"RED", "GREEN", "YELLOW", "BLUE"};
+                // Color color = Color.valueOf(colors[colorOption - 1]);
+                // writer = new MyConsoleWriter(color);
+            }
 
-        
+            // writer.write(msg);
+            System.out.println();
+        }while (true);
     }
 }
