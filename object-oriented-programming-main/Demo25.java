7f42c042100af90f54ac6012158d5934cd45726e
Kavindu95 <kavindueng95@gmail.com>
2023-09-12 19:06:23 +0530

    initial commit

--- object-oriented-programming-main/Demo25.java
+++ object-oriented-programming-main/Demo25.java
@@ -2,84 +2,85 @@ import java.time.LocalDateTime;
 
 public class Demo25 {
     public static void main(String[] args) {
-        User admin = new Admin("Kavindu95", "1995");
-        User admin2 = new Admin("Osi", "1996");
-        User guestUser = new GuestUser("Kasun", "1994");
+        User adminUser = new AdminUser("Prasad", "IJSE_123456");
+        User adminUser2 = new AdminUser("Yasendra", "4567891");
+        User guestUser = new GuestUser("John", "45612378");
 
-        admin.login();
-        admin2.login();
+        adminUser.login();
+        adminUser2.login();
         guestUser.login();
+
+        // new User();
     }
+}
+
+enum UserRole {
+    ADMIN, GUEST;
 }
 
 abstract class User {
     String username;
     String password;
-    String role;
+    UserRole role;
     boolean logged;
 
     public abstract void login();
 
-    public void logout() {
-        if (logged) {
+    public void logout(){
+        if (logged){
             System.out.printf("%s logged out \n", username);
-        } else {
-            System.out.println("user hasn't logged");
+        }else{
+            System.out.println("User hasn't logged in yet");
         }
-
+    } 
+    
+    public void printUserDetails(){
+        System.out.printf("username=%s, password=%s, role=%s \n",
+                username, password, role);
     }
-
-    public void printUserDetails() {
-        System.out.prinf("username=%s, password=%s,role=%s \n", username, password, role);
-    }
-
 }
 
-class Admin extends User {
-    private String[] passwords = { "IJSE_123", "IJSE_345" };
+class ITUser extends User {
+    public void login(){
 
-    public Admin(String username, String password) {
+    }
+}
+
+class AdminUser extends User{
+    private String[] passwords = {"IJSE_123456", "IJSE_4567891"};
+
+    public AdminUser(String username, String password){
         this.username = username;
         this.password = password;
-        this.role = "ROLE_ADMIN";
+        this.role = UserRole.ADMIN;
     }
-
     @Override
-    public void login() {
+    public void login(){
         for (String password : passwords) {
-            if (password.equals(this.password)) {
-                System.out.printf("%s logged in at: %s \n", username, LocalDateTime.now());
-
+            if (password.equals(this.password)){
+                System.out.printf("%s logged in at: %s \n",
+                         username, LocalDateTime.now());
                 return;
             }
-
         }
-        System.out.println("Invalid login Credentials");
+        System.out.println("Invalid login credentails");
     }
 }
 
-class GuestUser extends User {
-    private static final String[] GUEST_PW = { "IJSE_123456" };
-
-    public GuestUser(String username, String password) {
+class GuestUser extends User{
+    private static final String GUEST_PASSWORD = "GUEST_123456";
+    public GuestUser(String username, String password){
         this.username = username;
         this.password = password;
-        this.role = "ROLE_GUEST";
+        this.role = UserRole.GUEST;
     }
-
     @Override
-    public void login() {
-        for (String password : GUEST_PW) {
-            if (password.equals(GUEST_PW))
-
-            {
-                System.out.printf("%s logged in at: %s \n", username, LocalDateTime.now());
-
-                return;
-            } else {
-                System.out.println("Invalid login Credentials");
-            }
-
+    public void login(){
+        if (password.equals(GUEST_PASSWORD)){
+            System.out.printf("Guest User: %s logged in at: %s \n", 
+                            username,LocalDateTime.now());
+        }else{
+            System.out.println("Invalid login credentails");
         }
     }
 }
